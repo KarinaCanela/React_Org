c5c20d150b6fea97a87b5da6c336d2f21369d4bc
karina <kcanelacobos@gmail.com>
2023-04-13 17:31:48 -0600

    Agregando extension react developer tools

--- src/App.js
+++ src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/header/Header.js';
-console.log(Header)
 
 function App() {
 
   return (
-    <div className="App">
+    <div>
       {/* { Header() }
       <Header> </Header> */}
       <Header/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola mundo con React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende React
-        </a>
-      </header>
+      
     </div>
   );
 }
