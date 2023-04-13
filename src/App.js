bcf4990e6409ea85121f88c79348cb44dddb3d42
karina <kcanelacobos@gmail.com>
2023-04-13 17:17:42 -0600

    Usando componente Header

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Header from './componentes/header/Header.js';
 console.log(Header)
 
 function App() {
+
   return (
     <div className="App">
-      
+      {/* { Header() }
+      <Header> </Header> */}
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
--- src/componentes/header/Header.js
+++ src/componentes/header/Header.js
@@ -1,5 +1,9 @@
+import "./Header.css"
+
 function Header () {
-    return <img src="/img/header.png" alt='Org' />
+    return <header className="header">
+        <img src="/img/header.png" alt='Org' />
+    </header> 
 }
 
 export default Header
