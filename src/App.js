c88557741edbcbee368319d1def997a27ff4e581
karina <kcanelacobos@gmail.com>
2023-05-05 20:03:34 -0600

    Parte1 estado

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Header from './componentes/header/Header.js';
 import Formulario from './componentes/formulario/Formulario.js';
+import MiOrg from './componentes/MiOrg';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header> </Header> */}
       <Header/>
       <Formulario/>
+      <MiOrg/>
       
     </div>
   );
--- src/componentes/MiOrg/index.js
+++ src/componentes/MiOrg/index.js
@@ -1,5 +1,23 @@
+import { useState } from "react"
+import "./MiOrg.css"
+
 const MiOrg = () => {
-    return <section>
-        
+    //Estado - hooks
+    //useState
+    //useState()
+    //const [nombreVariable,funcionActualiza] = useState(valorInicial)
+
+    const [mostrar, actualizarMostrar] = useState(true)
+    const manejarClick = () => {
+        console.log("Mostrar/ocultar elemento", !mostrar)
+        actualizarMostrar(!mostrar)
+    }
+
+    return <section className="orgSection">
+        <h3 className="title">Mi organización</h3>
+        <img src="/img/add.png" alt="add" onClick={manejarClick} />
     </section>
 }
+
+export default MiOrg
+
