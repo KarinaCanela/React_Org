84f2926c08d19416129d9cb7f798574c217361e5
karina <kcanelacobos@gmail.com>
2023-05-19 21:03:38 -0600

    Instalando iconos

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,10 +1,11 @@
 import "./Colaborador.css"
+import { AiFillDelete } from "react-icons/ai"
 
 const Colaborador = (props) => {
     const {nombre, puesto, foto, equipo} = props.datos
     const {colorPrimario,eliminarColaborador} = props
     return <div className="colaborador">
-        <span onClick={eliminarColaborador}>D</span>
+        <AiFillDelete className="eliminar" onClick={eliminarColaborador}/>
         <div className="encabezado" style={{backgroundColor: colorPrimario
         }}>
             <img src={foto} alt={nombre}/>
